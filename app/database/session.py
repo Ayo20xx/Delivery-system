655dfa239525746f9ad67df2fe2e513badd5a868
Ayo20xx <ayomidevictoraaaa@gmail.com>
2026-05-07 14:15:36 +0100

    updated gitignore and added sessionns and models

---
+++ app/database/session.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlmodel import Session, SQLModel
+
+
+engine=create_engine(
+    url="sqlite:///sqlite.db",
+    echo=True,
+    connect_args={"check_same_thread": False },
+
+)
+
+
+def create_db_tables():
+    from .model import Shipment # noqa: F401
+    SQLModel.metadata.create_all(bind=engine)
+
+def get_session():
+    with Session(bind=engine) as session:
+        yield session
+SessionDep=Annotated[Session,Depends(get_session)]
